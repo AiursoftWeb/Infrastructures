443d175e604a393b40f7164bd7bece1f84ad1b15
Anduin2017 <Anduin@aiursoft.com>
2018-04-16 16:53:47 +0800

    Using release mode

--- backend.sh
+++ backend.sh
@@ -5,43 +5,43 @@ dotnet build
 cd ..
 
 cd API
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd Developer
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd OSS
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd Account
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd Stargate
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd Kahla.Server
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd Kahla.Home
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd Wiki
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd EE
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 cd Colossus
-dotnet publish --no-restore
+dotnet publish --no-restore -c Release
 cd ..
 
 exit
