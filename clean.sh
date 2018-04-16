1829177e876c54abc65230d63e1d5a5263139a00
Anduin2017 <Anduin@aiursoft.com>
2018-04-16 18:02:41 +0800

    Use name as nickname not email

--- clean.sh
+++ clean.sh
@@ -2,6 +2,7 @@ echo Start cleaning...
 find . -name 'node_modules' | xargs -L1 rm -rvf
 find . -name 'bin' | xargs -L1 rm -rvf
 find . -name 'obj' | xargs -L1 rm -rvf
+rm ./.vs -rvf
 rm ./Kahla.App/www -rvf
 rm ./Kahla.App/dist -rvf
 rm ./CDN/dist -rvf
