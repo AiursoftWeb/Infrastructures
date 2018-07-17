0ae355757f18d886e48eff2900b98c1e51b9ace9
Anduin2017 <Anduin@aiursoft.com>
2018-07-17 17:19:44 +0800

    Create new section function is production level.

--- EE/Models/SectionViewModels/CreateViewModel.cs
+++ EE/Models/SectionViewModels/CreateViewModel.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Aiursoft.EE.Models.SectionViewModels
 {
     public class CreateViewModel
     {
+        [Required]
+        [Display(Name = "New Section Name")]
+        [MinLength(5)]
+        [MaxLength(30)]
         public string NewSectionName { get; set; }
         public string CourseName { get; set; }
         public int CourseId { get; set; }
