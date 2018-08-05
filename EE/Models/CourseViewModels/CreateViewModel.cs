05ac5b0413fe55ea5e429d34f6289ccb2864580c
Anduin2017 <Anduin@aiursoft.com>
2018-08-05 20:32:42 +0800

    Fix #46

--- EE/Models/CourseViewModels/CreateViewModel.cs
+++ EE/Models/CourseViewModels/CreateViewModel.cs
@@ -11,9 +11,10 @@ namespace Aiursoft.EE.Models.CourseViewModels
         [Required]
         public string Name { get; set; }
         [Required]
+        [Display(Name = "Describe your course about what will be talked about")]
         public string Description { get; set; }
         [Required]
-        [Display(Name = "What you will learn")]
+        [Display(Name = "What will be learned from this course")]
         public string WhatYouWillLearn { get; set; }
         public double Price { get; set; }
         public bool DisplayOwnerInfo { get; set; } = true;
