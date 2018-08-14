294a554731034cdd28610b122af867076dcbf30d
Anduin2017 <Anduin@aiursoft.com>
2018-08-14 13:33:09 +0800

    Will log when creating HTTP requests.

--- Pylon/Services/HTTPService.cs
+++ Pylon/Services/HTTPService.cs
@@ -1,4 +1,5 @@
 ﻿using Aiursoft.Pylon.Models;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,41 +13,55 @@ namespace Aiursoft.Pylon.Services
 {
     public class HTTPService
     {
-        public HTTPService()
+        private readonly ILogger _logger;
+        private readonly CookieContainer _cc;
+        public HTTPService(ILogger<HTTPService> logger)
         {
-
+            _logger = logger;
+            _cc = new CookieContainer();
         }
-        public CookieContainer CC = new CookieContainer();
 
-        public async Task<string> Get(AiurUrl Url, bool internalRequest = false)
+        public async Task<string> Get(AiurUrl url, bool internalRequest = false)
         {
-            var request = WebRequest.CreateHttp(Url.ToString());
+            HttpWebRequest request = null;
             if (internalRequest)
             {
+                url.Address = url.Address.Replace("https://", "http://");
+                request = WebRequest.CreateHttp(url.ToString());
                 request.Headers.Add("x-forwarded-proto", "INTERNAL");
-                Url.Address = Url.Address.Replace("https://", "http://");
             }
-            request.CookieContainer = CC;
+            else
+            {
+                request = WebRequest.CreateHttp(url.ToString());
+            }
+            _logger.LogInformation($"Creating HTTP GET request to: {request.RequestUri.ToString()}");
+            request.CookieContainer = _cc;
             request.Method = "GET";
             request.ContentType = "text/html;charset=utf-8";
             return await HTTPMethods.ReadFromResponseAsync(request);
         }
-        public async Task<string> Post(AiurUrl Url, AiurUrl postDataStr, bool internalRequest = false)
+        public async Task<string> Post(AiurUrl url, AiurUrl postDataStr, bool internalRequest = false)
         {
-            var request = WebRequest.CreateHttp(Url.ToString());
+            HttpWebRequest request = null;
             if (internalRequest)
             {
+                url.Address = url.Address.Replace("https://", "http://");
+                request = WebRequest.CreateHttp(url.ToString());
                 request.Headers.Add("x-forwarded-proto", "INTERNAL");
-                Url.Address = Url.Address.Replace("https://", "http://");
             }
-            request.CookieContainer = CC;
+            else
+            {
+                request = WebRequest.CreateHttp(url.ToString());
+            }
+            _logger.LogInformation($"Creating HTTP Post request to: {request.RequestUri.ToString()}");
+            request.CookieContainer = _cc;
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
             await HTTPMethods.SendRequestAsync(request, postDataStr.ToString().TrimStart('?'));
             return await HTTPMethods.ReadFromResponseAsync(request);
         }
 
-        public async Task<string> PostFile(AiurUrl Url, string filepath)
+        public async Task<string> PostFile(AiurUrl url, string filepath)
         {
             var request = new HttpClient();
             var form = new MultipartFormDataContent();
@@ -60,7 +75,7 @@ namespace Aiursoft.Pylon.Services
                 }
                 memory.Position = 0;
                 form.Add(new StreamContent(memory), "file", new FileInfo(filepath).FullName);
-                response = await request.PostAsync(Url.ToString(), form);
+                response = await request.PostAsync(url.ToString(), form);
                 memory.Close();
             }
             return await response.Content.ReadAsStringAsync();
