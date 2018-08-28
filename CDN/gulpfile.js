ad29a06b6d5834e5b7a7df652a828e1c210762c4
Anduin2017 <anduin@aiursoft.com>
2018-08-28 14:53:27 +0800

    Use back full version of jquery.

--- CDN/gulpfile.js
+++ CDN/gulpfile.js
@@ -30,7 +30,7 @@ var packages = [
     // AiurCore - JS
     {
         inputFiles: [
-            'node_modules/jquery/dist/jquery.slim.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
             'node_modules/popper.js/dist/umd/popper.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
             'node_modules/jquery-validation/dist/jquery.validate.min.js',
