c1e06f22dc586d5374713409e07b2366ade221a7
Anduin2017 <anduin@aiursoft.com>
2018-08-28 13:40:12 +0800

    move something to aiurmarket

---
+++ Colossus/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Aiursoft.Pylon.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Colossus.Controllers
+{
+    [AiurForceAuth]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
