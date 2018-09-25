fbaf8ff72cfc820bf4966a9326e3e12a9a0d2e3c
Anduin2017 <Anduin@aiursoft.com>
2018-09-25 23:30:34 +0800

    Create a loading image.

--- Pylon/Values.cs
+++ Pylon/Values.cs
@@ -18,7 +18,7 @@ namespace Aiursoft.Pylon
     {
         public static string ProjectName = "Aiursoft";
         public static int DefaultImageId = 10;
-        public static string LoadingImage = "https://";
+        public static string LoadingImage = "https://cdn.aiursoft.com/images/loading.gif";
         public static string CorpPhoneNumber = "(+86) 8368-5000";
         public static string Schema = "https";
         public static string WSSchema = "wss";
