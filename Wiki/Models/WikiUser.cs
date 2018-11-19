8041d9be59513a15a2921b5880ed7b4ee4b4a9f6
Anduin2017 <anduin@aiursoft.com>
2018-11-19 14:32:46 +0800

    Mark doc API address not mapped.

--- Wiki/Models/WikiUser.cs
+++ Wiki/Models/WikiUser.cs
@@ -22,6 +22,7 @@ namespace Aiursoft.Wiki.Models
         public string CollectionTitle { get; set; }
         [InverseProperty(nameof(Article.Collection))]
         public List<Article> Articles { get; set; }
+        [NotMapped]
         public string DocAPIAddress { get; set; }
     }
     public class Article
