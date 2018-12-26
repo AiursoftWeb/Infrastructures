dee66d1d3afef67bed9a51130291c84f6902d8db
Anduin2017 <anduin@aiursoft.com>
2018-12-26 16:55:37 +0800

    Create first tool.

---
+++ Developer/Controllers/ToolsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Developer.Controllers
+{
+    public class ToolsController : Controller
+    {
+        public IActionResult WebSocket()
+        {
+            return View();
+        }
+    }
+}
