6c36f4163176226b3d6d5fbd90e31cb6a5747485
Anduin2017 <Anduin@aiursoft.com>
2019-03-14 23:40:23 +0900

    Build archon.

--- Archon/Controllers/HomeController.cs
+++ Archon/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Aiursoft.Archon.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using Aiursoft.Pylon.Services;
 using Aiursoft.Pylon;
 using Aiursoft.Pylon.Models;
