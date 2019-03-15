67cd137843602d5c3d99656d666521a0a758b975
Anduin2017 <anduin@aiursoft.com>
2019-03-15 11:46:18 +0900

    better clean scripts.

--- clean.sh
+++ clean.sh
@@ -1,8 +1,5 @@
 echo Start cleaning...
-find . -name 'node_modules' | xargs -L1 rm -rvf
 find . -name 'bin' | xargs -L1 rm -rvf
 find . -name 'obj' | xargs -L1 rm -rvf
 rm ./.vs -rvf
-rm ./CDN/dist -rvf
-rm ./CDN/fonts -rvf
 exit
