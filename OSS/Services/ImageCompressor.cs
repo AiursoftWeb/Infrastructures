56e16c22aa4a402c1037340b900a35cafc4cb9e7
Anduin2017 <anduin@aiursoft.com>
2019-04-08 12:49:59 +0900

    Handle ImageFormatException

--- OSS/Services/ImageCompressor.cs
+++ OSS/Services/ImageCompressor.cs
@@ -19,16 +19,24 @@ namespace Aiursoft.OSS.Services
 
         public async Task<string> Compress(string path, string realname, int width, int height)
         {
-            var compressedFolder = _configuration["StoragePath"] + $"{Path.DirectorySeparatorChar}Compressed{Path.DirectorySeparatorChar}";
-            if (Directory.Exists(compressedFolder) == false)
+            try
             {
-                Directory.CreateDirectory(compressedFolder);
+                var compressedFolder = _configuration["StoragePath"] + $"{Path.DirectorySeparatorChar}Compressed{Path.DirectorySeparatorChar}";
+                if (Directory.Exists(compressedFolder) == false)
+                {
+                    Directory.CreateDirectory(compressedFolder);
+                }
+                var compressedImagePath = $"{compressedFolder}oss_compressed_w{width}h{height}{realname}";
+                await GetReducedImage(path, compressedImagePath, width, height);
+                return compressedImagePath;
             }
-            var compressedImagePath = $"{compressedFolder}oss_compressed_w{width}h{height}{realname}";
-            await GetReducedImage(path, compressedImagePath, width, height);
-            return compressedImagePath;
+            catch (ImageFormatException)
+            {
+                return path;
+            }
         }
-        public async Task GetReducedImage(string sourceImage, string saveTarget, int width, int height)
+
+        private async Task GetReducedImage(string sourceImage, string saveTarget, int width, int height)
         {
             var sourceFileInfo = new FileInfo(sourceImage);
             if (File.Exists(saveTarget))
