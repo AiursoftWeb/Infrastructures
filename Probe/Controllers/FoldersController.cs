a544d6363874c5ddeceeee24f9cfa21f8e989c2b
Anduin Xue <anduin@aiursoft.com>
2019-04-23 19:07:28 +0900

    Create empty folder controller.

---
+++ Probe/Controllers/FoldersController.cs
@@ -0,0 +1,24 @@
+﻿using Aiursoft.Pylon.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Probe.Controllers
+{
+    [APIExpHandler]
+    [APIModelStateChecker]
+    public class FoldersController : Controller
+    {
+        public FoldersController()
+        {
+
+        }
+
+        //public async Task<IActionResult> ViewContent(string siteName, string folderName)
+        //{
+
+        //}
+    }
+}
