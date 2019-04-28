e273d5645e669a30034077096f4c58ca98e0afaf
Anduin Xue <anduin@aiursoft.com>
2019-04-28 19:36:13 +0800

    Create new files controller.

---
+++ Probe/Controllers/FilesController.cs
@@ -0,0 +1,42 @@
+﻿using Aiursoft.Probe.Data;
+using Aiursoft.Probe.Services;
+using Aiursoft.Pylon.Attributes;
+using Aiursoft.Pylon.Models.Probe.FilesAddressModels;
+using Aiursoft.Pylon.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Probe.Controllers
+{
+    [APIExpHandler]
+    [APIModelStateChecker]
+    [Route("Files")]
+    public class FilesController
+    {
+        private readonly ProbeDbContext _dbContext;
+        private readonly ACTokenManager _tokenManager;
+        private readonly FolderLocator _folderLocator;
+
+        public FilesController(
+            ProbeDbContext dbContext,
+            ACTokenManager tokenManager,
+            FolderLocator folderLocator)
+        {
+            _dbContext = dbContext;
+            _tokenManager = tokenManager;
+            _folderLocator = folderLocator;
+        }
+
+        [HttpPost]
+        [Route("UploadFile/{SiteName}/{**FolderNames}")]
+        [FileChecker]
+        public async Task<IActionResult> UploadFile(UploadFileAddressModel model)
+        {
+            await Task.Delay(0);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pylon/Models/Probe/FilesAddressModels/UploadFileAddressModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Aiursoft.Pylon.Models.Probe.FilesAddressModels
+{
+    public class UploadFileAddressModel
+    {
+        [Required]
+        public string AccessToken { get; set; }
+        [Required]
+        public string SiteName { get; set; }
+        public string FolderNames { get; set; }
+    }
+}
