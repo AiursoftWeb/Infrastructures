4cb4e33a725a8e3d27a275feaf1ec57b649cd30f
Anduin Xue <anduin@aiursoft.com>
2019-05-15 16:39:29 +0800

    Really create site.

--- Developer/Controllers/SitesController.cs
+++ Developer/Controllers/SitesController.cs
@@ -1,6 +1,9 @@
 ﻿using Aiursoft.Developer.Data;
 using Aiursoft.Developer.Models.SitesViewModels;
+using Aiursoft.Pylon.Exceptions;
 using Aiursoft.Pylon.Models.Developer;
+using Aiursoft.Pylon.Services;
+using Aiursoft.Pylon.Services.ToProbeServer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,9 +15,17 @@ namespace Aiursoft.Developer.Controllers
     public class SitesController : Controller
     {
         public DeveloperDbContext _dbContext;
-        public SitesController(DeveloperDbContext dbContext)
+        private readonly AppsContainer _appsContainer;
+        private readonly SitesService _sitesService;
+
+        public SitesController(
+            DeveloperDbContext dbContext,
+            AppsContainer appsContainer,
+            SitesService sitesService)
         {
             _dbContext = dbContext;
+            _appsContainer = appsContainer;
+            _sitesService = sitesService;
         }
 
         public async Task<IActionResult> Index()
@@ -32,6 +43,37 @@ namespace Aiursoft.Developer.Controllers
                 AppId = id
             };
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateSite(CreateSiteViewModel model)
+        {
+            var user = await GetCurrentUserAsync();
+            if (!ModelState.IsValid)
+            {
+                model.ModelStateValid = false;
+                model.Recover(user);
+                return View(model);
+            }
+            var app = await _dbContext.Apps.FindAsync(model.AppId);
+            if (app == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                var token = await _appsContainer.AccessToken(app.AppId, app.AppSecret);
+                await _sitesService.CreateNewSiteAsync(token, model.SiteName);
+                return RedirectToAction(nameof(AppsController.ViewApp), "Apps", new { id = app.AppId, JustHaveUpdated = true });
+            }
+            catch (AiurUnexceptedResponse e)
+            {
+                ModelState.AddModelError(string.Empty, e.Response.Message);
+                model.ModelStateValid = false;
+                model.Recover(user);
+                return View(model);
+            }
         }
 
         private async Task<DeveloperUser> GetCurrentUserAsync()
