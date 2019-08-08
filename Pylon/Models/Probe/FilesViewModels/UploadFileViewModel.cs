ce2bd1a45e5945d5d1823d5611bae82a6e126605
Anduin2017 <anduin@aiursoft.com>
2019-08-08 13:22:02 +0800

    Update some document.

--- Pylon/Models/Probe/FilesViewModels/UploadFileViewModel.cs
+++ Pylon/Models/Probe/FilesViewModels/UploadFileViewModel.cs
@@ -12,12 +12,12 @@ namespace Aiursoft.Pylon.Models.Probe.FilesViewModels
         public string SiteName { get; set; }
 
         /// <summary>
-        /// For example: pathA/pathB/file.extension.
+        /// For example: pathA/pathB/file.extension. Caution: This property is not UrlEncoded!
         /// </summary>
         public string FilePath { get; set; }
 
         /// <summary>
-        /// For example: https://probe.aiursoft.com/Download/InSites/mynewsite/img_20190727_143308.jpg
+        /// For example: https://probe.aiursoft.com/Download/InSites/mynewsite/img_20190727_143308.jpg. Caution: This property is encoded and you can access it directly.
         /// </summary>
         public string InternetPath { get; set; }
     }
