63a607e7d201084df8938954a8f0abfd6b5967e0
c <15657868169m@sina.cn>
2019-09-07 01:00:37 +0800

    Fixed Clear button no use bug

---
+++ Colossus/Scripts/jquery.table.rowspan.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    $.fn.extend({
+        //表格合并单元格，colIdx要合并的列序号，从0开始
+        "rowspan": function (colIdx) {
+            return this.each(function () {
+                var that;
+                $('tr', this).each(function (row) {
+                    $('td:eq(' + colIdx + ')', this).filter(':visible').each(function (col) {
+                        if (that != null && $(this).html() == $(that).html()) {
+                            rowspan = $(that).attr("rowSpan");
+                            if (rowspan == undefined) {
+                                $(that).attr("rowSpan", 1);
+                                rowspan = $(that).attr("rowSpan");
+                            }
+                            rowspan = Number(rowspan) + 1;
+                            $(that).attr("rowSpan", rowspan);
+                            $(this).hide();
+                        } else {
+                            that = this;
+                        }
+                    });
+                });
+            });
+        }
+    });
+
+})(jQuery);
