9fa7ecf8774333cf18aefe495ec8034ff36dcbda
Anduin2017 <anduin@aiursoft.com>
2019-09-09 13:57:29 +0800

    Remove two obsolete methods.

--- Pylon/Services/ToAPIServer/AccountService.cs
+++ Pylon/Services/ToAPIServer/AccountService.cs
@@ -20,39 +20,6 @@ namespace Aiursoft.Pylon.Services.ToAPIServer
             _http = http;
         }
 
-        public async Task<AiurValue<int>> PasswordAuthAsync(string accessToken, string email, string password)
-        {
-            var url = new AiurUrl(_serviceLocation.API, "Account", "PasswordAuth", new { });
-            var form = new AiurUrl(string.Empty, new PasswordAuthAddressModel
-            {
-                AccessToken = accessToken,
-                Email = email,
-                Password = password
-            });
-            var result = await _http.Post(url, form, true);
-            var jresult = JsonConvert.DeserializeObject<AiurValue<int>>(result);
-            if (jresult.Code != ErrorType.Success)
-                throw new AiurUnexceptedResponse(jresult);
-            return jresult;
-        }
-
-        public async Task<AiurProtocol> AppRegisterAsync(string accessToken, string email, string password, string confirmPassword)
-        {
-            var url = new AiurUrl(_serviceLocation.API, "Account", "AppRegister", new { });
-            var form = new AiurUrl(string.Empty, new AppRegisterAddressModel
-            {
-                AccessToken = accessToken,
-                Email = email,
-                Password = password,
-                ConfirmPassword = confirmPassword
-            });
-            var result = await _http.Post(url, form, true);
-            var jresult = JsonConvert.DeserializeObject<AiurProtocol>(result);
-            if (jresult.Code != ErrorType.Success)
-                throw new AiurUnexceptedResponse(jresult);
-            return jresult;
-        }
-
         public async Task<CodeToOpenIdViewModel> CodeToOpenIdAsync(string accessToken, int code)
         {
             var url = new AiurUrl(_serviceLocation.API, "Account", "CodeToOpenId", new CodeToOpenIdAddressModel
