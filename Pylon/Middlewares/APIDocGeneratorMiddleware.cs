aa057a800be8d4d9956c679e3a38893c585f9373
Anduin2017 <anduin@aiursoft.com>
2019-09-09 14:45:35 +0800

    Rename a method.

--- Pylon/Middlewares/APIDocGeneratorMiddleware.cs
+++ Pylon/Middlewares/APIDocGeneratorMiddleware.cs
@@ -48,7 +48,7 @@ namespace Aiursoft.Pylon.Middlewares
                         continue;
                     }
                     var args = GenerateArguments(method);
-                    var possibleResponses = PossibleResponses(method);
+                    var possibleResponses = GetPossibleResponses(method);
                     var api = new API
                     {
                         ControllerName = controller.Name,
@@ -66,7 +66,7 @@ namespace Aiursoft.Pylon.Middlewares
             return;
         }
 
-        private string[] PossibleResponses(MethodInfo action)
+        private string[] GetPossibleResponses(MethodInfo action)
         {
             try
             {
