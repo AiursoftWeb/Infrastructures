9e312a8b69c56d98a7c3518e0a853af7716e8075
Anduin2017 <anduin@aiursoft.com>
2019-09-19 16:11:18 +0800

    Remove a jquery example.

--- Developer/Controllers/SamplesController.cs
+++ Developer/Controllers/SamplesController.cs
@@ -1,10 +1,6 @@
 ﻿using Aiursoft.Developer.Models.SamplesViewModels;
 using Aiursoft.Pylon.Attributes;
-using Aiursoft.Pylon.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Aiursoft.Developer.Controllers
@@ -12,16 +8,6 @@ namespace Aiursoft.Developer.Controllers
     [LimitPerMin]
     public class SamplesController : Controller
     {
-        private readonly StorageService _storageService;
-        private readonly IConfiguration _configuration;
-        public SamplesController(
-            StorageService storageService,
-            IConfiguration configuration)
-        {
-            _storageService = storageService;
-            _configuration = configuration;
-        }
-
         public ActionResult DisableWithForm()
         {
             var model = new DisableWithFormViewModel();
@@ -34,39 +20,6 @@ namespace Aiursoft.Developer.Controllers
         {
             await Task.Delay(4000);
             return RedirectToAction(nameof(DisableWithForm));
-        }
-
-        public IActionResult FormSample()
-        {
-            var model = new FormSampleViewModel();
-            return View(model);
-        }
-
-        [HttpPost]
-        [APIExpHandler]
-        [FileChecker(MaxSize = 30 * 1024 * 1024)]
-        [APIModelStateChecker]
-        public async Task<IActionResult> Uploader()
-        {
-            if (HttpContext.Request.Form.Files.First().Length > 30 * 1024 * 1024)
-            {
-                return Unauthorized();
-            }
-            var file = Request.Form.Files.First();
-            var model = await _storageService
-                .SaveToProbe(file, _configuration["SampleSiteName"], DateTime.UtcNow.ToString("yyyy-MM-dd"), SaveFileOptions.SourceName);
-            return Json(new
-            {
-                message = "Uploaded!",
-                value = model.InternetPath
-            });
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult SubmitForm(FormSampleViewModel model)
-        {
-            return View(model);
         }
 
         public IActionResult UTCTime()
