314ec70ed39c0262a1a4f15a126c3b1dd865a1d7
Anduin2017 <anduin@aiursoft.com>
2019-10-15 16:22:54 +0800

    Use our own mail server.

--- Pylon/Services/AiurEmailSender.cs
+++ Pylon/Services/AiurEmailSender.cs
@@ -1,31 +1,44 @@
 ﻿using Aiursoft.Pylon.Interfaces;
 using Microsoft.Extensions.Configuration;
-using SendGrid;
-using SendGrid.Helpers.Mail;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Aiursoft.Pylon.Services
 {
     public class AiurEmailSender : ITransientDependency
     {
-        private readonly IConfiguration _configuration;
+        private readonly string _mailFrom;
+        private readonly string _mailUser;
+        private readonly string _mailPassword;
+        private readonly string _mailServer;
 
         public AiurEmailSender(IConfiguration configuration)
         {
-            _configuration = configuration;
+            _mailFrom = configuration["MailFrom"];
+            _mailUser = configuration["MailUser"];
+            _mailPassword = configuration["MailPassword"];
+            _mailServer = configuration["MailServer"];
         }
 
         public async Task SendEmail(string target, string targetsubject, string content)
         {
-            var key = _configuration["SendGridAPIKey"];
-            var apiKey = key;
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("service@aiursoft.com", "Aiursoft User Service");
-            var subject = targetsubject;
-            var to = new EmailAddress(target);
-            var htmlContent = content;
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, string.Empty, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+            var client = new SmtpClient(_mailServer)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_mailUser, _mailPassword),
+                EnableSsl = true,
+                Port = 587
+            };
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_mailFrom),
+                Body = content,
+                Subject = targetsubject,
+                IsBodyHtml = true
+            };
+            mailMessage.To.Add(target);
+            await client.SendMailAsync(mailMessage);
         }
     }
 }
