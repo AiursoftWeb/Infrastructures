a16ad13a19dcbc11d5c7ebce4c7c34b1bd0a39ff
Anduin2017 <anduin@aiursoft.com>
2019-10-19 15:31:51 +0900

    Directly sign in if already bind.

--- Gateway/Controllers/ThirdPartyController.cs
+++ Gateway/Controllers/ThirdPartyController.cs
@@ -1,6 +1,10 @@
-﻿using Aiursoft.Gateway.Models.ThirdPartyAddressModels;
+﻿using Aiursoft.Gateway.Data;
+using Aiursoft.Gateway.Models.ThirdPartyAddressModels;
+using Aiursoft.Gateway.Services;
+using Aiursoft.Pylon.Models;
 using Aiursoft.Pylon.Services.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,21 +16,39 @@ namespace Aiursoft.Gateway.Controllers
     public class ThirdPartyController : Controller
     {
         private readonly IEnumerable<IAuthProvider> _authProviders;
+        private readonly GatewayDbContext _dbContext;
+        private readonly AuthFinisher _authFinisher;
 
-        public ThirdPartyController(IEnumerable<IAuthProvider> authProviders)
+        public ThirdPartyController(
+            IEnumerable<IAuthProvider> authProviders,
+            GatewayDbContext dbContext,
+            AuthFinisher authFinisher)
         {
             _authProviders = authProviders;
+            _dbContext = dbContext;
+            _authFinisher = authFinisher;
         }
 
         [Route("Sign-in/{providerName}")]
         public async Task<IActionResult> SignIn(SignInAddressModel model)
         {
             var provider = _authProviders.SingleOrDefault(t => t.GetName().ToLower() == model.ProviderName.ToLower());
-            if(provider == null)
+            if (provider == null)
             {
                 return NotFound();
             }
+            var oauthModel = model.BuildOAuthInfo();
             var info = await provider.GetUserDetail(model.Code);
+            var account = await _dbContext
+                .ThirdPartyAccounts
+                .Include(t => t.Owner)
+                .ThenInclude(t => t.Emails)
+                .SingleOrDefaultAsync(t => t.OpenId == info.Id.ToString());
+            if (account != null)
+            {
+                oauthModel.Email = account.Owner.Email;
+                await _authFinisher.FinishAuth(this, oauthModel);
+            }
             return Json(info);
         }
     }
--- Gateway/Models/ThirdPartyAddressModels/SignInAddressModel.cs
+++ Gateway/Models/ThirdPartyAddressModels/SignInAddressModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Aiursoft.Pylon.Exceptions;
+using Aiursoft.Pylon.Models;
+using Aiursoft.Pylon.Models.API.OAuthAddressModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Aiursoft.Gateway.Models.ThirdPartyAddressModels
@@ -10,5 +14,29 @@ namespace Aiursoft.Gateway.Models.ThirdPartyAddressModels
         public string ProviderName { get; set; }
         public string Code { get; set; }
         public string State { get; set; }
+
+        public OAuthInfo BuildOAuthInfo()
+        {
+            try
+            {
+                var values = State
+                    .TrimStart('?')
+                    .Split('&')
+                    .Select(t => t.Split('='))
+                    .Select(t => new KeyValuePair<string, string>(t[0].ToLower(), WebUtility.UrlDecode(t[1])));
+                return new OAuthInfo
+                {
+                    AppId = values.SingleOrDefault(t => t.Key == nameof(AuthorizeAddressModel.appid)).Value,
+                    ToRedirect = values.SingleOrDefault(t => t.Key == nameof(AuthorizeAddressModel.redirect_uri).ToLower()).Value,
+                    State = values.SingleOrDefault(t => t.Key == nameof(AuthorizeAddressModel.state).ToLower()).Value,
+                    Scope = values.SingleOrDefault(t => t.Key == nameof(AuthorizeAddressModel.scope).ToLower()).Value,
+                    ResponseType = values.SingleOrDefault(t => t.Key == nameof(AuthorizeAddressModel.response_type).ToLower()).Value,
+                };
+            }
+            catch (Exception e) when (e is IndexOutOfRangeException || e is NullReferenceException)
+            {
+                throw new AiurAPIModelException(ErrorType.InvalidInput, "State is invalid!");
+            }
+        }
     }
 }
