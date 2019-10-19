c0296ac3a25920a4f0f8902eea5f35033de7fb75
Anduin2017 <anduin@aiursoft.com>
2019-10-19 14:48:06 +0900

    Create sign in function.

---
+++ Gateway/Controllers/ThirdPartyController.cs
@@ -0,0 +1,33 @@
+﻿using Aiursoft.Gateway.Models.ThirdPartyAddressModels;
+using Aiursoft.Pylon.Services.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Gateway.Controllers
+{
+    [Route("Third-party")]
+    public class ThirdPartyController : Controller
+    {
+        private readonly IEnumerable<IAuthProvider> _authProviders;
+
+        public ThirdPartyController(IEnumerable<IAuthProvider> authProviders)
+        {
+            _authProviders = authProviders;
+        }
+
+        [Route("Sign-in/{providerName}")]
+        public IActionResult SignIn(SignInAddressModel model)
+        {
+            var provider = _authProviders.SingleOrDefault(t => t.GetName().ToLower() == model.ProviderName.ToLower());
+            if(provider == null)
+            {
+                return NotFound();
+            }
+            var info = = provider.GetUserDetail(model.Code);
+            return Json(info);
+        }
+    }
+}
---
+++ Gateway/Models/ThirdPartyAddressModels/SignInAddressModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Gateway.Models.ThirdPartyAddressModels
+{
+    public class SignInAddressModel
+    {
+        public string ProviderName { get; set; }
+        public string Code { get; set; }
+        public string State { get; set; }
+    }
+}
