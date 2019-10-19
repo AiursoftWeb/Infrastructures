9aaa1f199bd090bb90b577b0adfeb4f872c572e0
Anduin2017 <anduin@aiursoft.com>
2019-10-20 00:32:17 +0900

    Really create a new account.

--- Gateway/Controllers/ThirdPartyController.cs
+++ Gateway/Controllers/ThirdPartyController.cs
@@ -1,11 +1,14 @@
 ﻿using Aiursoft.Gateway.Data;
+using Aiursoft.Gateway.Models;
 using Aiursoft.Gateway.Models.ThirdPartyAddressModels;
 using Aiursoft.Gateway.Models.ThirdyPartyViewModels;
 using Aiursoft.Gateway.Services;
+using Aiursoft.Pylon;
 using Aiursoft.Pylon.Exceptions;
 using Aiursoft.Pylon.Models;
 using Aiursoft.Pylon.Services.Authentication;
 using Aiursoft.Pylon.Services.ToDeveloperServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -22,17 +25,23 @@ namespace Aiursoft.Gateway.Controllers
         private readonly GatewayDbContext _dbContext;
         private readonly AuthFinisher _authFinisher;
         private readonly DeveloperApiService _apiService;
+        private readonly UserManager<GatewayUser> _userManager;
+        private readonly SignInManager<GatewayUser> _signInManager;
 
         public ThirdPartyController(
             IEnumerable<IAuthProvider> authProviders,
             GatewayDbContext dbContext,
             AuthFinisher authFinisher,
-            DeveloperApiService apiService)
+            DeveloperApiService apiService,
+            UserManager<GatewayUser> userManager,
+            SignInManager<GatewayUser> signInManager)
         {
             _authProviders = authProviders;
             _dbContext = dbContext;
             _authFinisher = authFinisher;
             _apiService = apiService;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [Route("Sign-in/{providerName}")]
@@ -84,11 +93,50 @@ namespace Aiursoft.Gateway.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
         [Route("create-account-and-bind/{providerName}")]
         public async Task<IActionResult> CreateAccountAndBind(SignInViewModel model)
         {
-            await Task.Delay(0);
-            throw new NotImplementedException();
+            bool exists = _dbContext.UserEmails.Any(t => t.EmailAddress == model.UserDetail.Email.ToLower());
+            if (exists)
+            {
+                throw new AiurAPIModelException(ErrorType.HasDoneAlready, $"An user with email '{model.UserDetail.Email}' already exists!");
+            }
+            var user = new GatewayUser
+            {
+                UserName = model.UserDetail.Email,
+                Email = model.UserDetail.Email,
+                NickName = model.UserDetail.Name,
+                PreferedLanguage = model.PreferedLanguage,
+                IconFilePath = Values.DefaultImagePath
+            };
+            var result = await _userManager.CreateAsync(user);
+            if (result.Succeeded)
+            {
+                var primaryMail = new UserEmail
+                {
+                    EmailAddress = model.UserDetail.Email.ToLower(),
+                    OwnerId = user.Id,
+                    ValidateToken = Guid.NewGuid().ToString("N")
+                };
+                _dbContext.UserEmails.Add(primaryMail);
+                await _dbContext.SaveChangesAsync();
+                await _signInManager.SignInAsync(user, isPersistent: true);
+                var log = new AuditLogLocal
+                {
+                    UserId = user.Id,
+                    IPAddress = HttpContext.Connection.RemoteIpAddress.ToString(),
+                    Success = true,
+                    AppId = model.OAuthInfo.AppId
+                };
+                _dbContext.AuditLogs.Add(log);
+                await _dbContext.SaveChangesAsync();
+                return await _authFinisher.FinishAuth(this, model.OAuthInfo);
+            }
+            else
+            {
+                throw new AiurAPIModelException(ErrorType.HasDoneAlready, result.Errors.First().Description);
+            }
         }
     }
 }
--- Gateway/Models/ThirdyPartyViewModels/SignInViewModel.cs
+++ Gateway/Models/ThirdyPartyViewModels/SignInViewModel.cs
@@ -1,21 +1,22 @@
-﻿using Aiursoft.Pylon.Attributes;
-using Aiursoft.Pylon.Models;
+﻿using Aiursoft.Pylon.Models;
 using Aiursoft.Pylon.Services.Authentication;
-using System;
-using System.Collections.Generic;
+using Aiursoft.Pylon.Services.Authentications.ToGitHubServer;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Aiursoft.Gateway.Models.ThirdyPartyViewModels
 {
     public class SignInViewModel
     {
+        public SignInViewModel()
+        {
+            UserDetail = new GitHubUserDetail();
+        }
         public OAuthInfo OAuthInfo { get; set; }
         public IUserDetail UserDetail { get; set; }
         public string ProviderName { get; set; }
         public string AppImageUrl { get; set; }
         public bool CanFindAnAccountWithEmail { get; set; }
-        public IAuthProvider Provider { get; internal set; }
+        public IAuthProvider Provider { get; set; }
+        public string PreferedLanguage { get; set; }
     }
 }
