2c2acf10931b3750c718d2cfd892616de382b272
Anduin2017 <anduin@aiursoft.com>
2019-10-25 01:37:27 +0900

    Bind account limit address model build.

--- Gateway/Models/ThirdPartyAddressModels/BindAccountAddressModel.cs
+++ Gateway/Models/ThirdPartyAddressModels/BindAccountAddressModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Aiursoft.Gateway.Models.ThirdPartyAddressModels
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aiursoft.Gateway.Models.ThirdPartyAddressModels
 {
     public class BindAccountAddressModel
     {
+        [FromRoute]
         public string ProviderName { get; set; }
+        [FromQuery(Name = "code")]
         public string Code { get; set; }
     }
 }
