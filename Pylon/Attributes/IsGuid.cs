bbd2974d142f178eb6e1297c7bece2cea5e6284a
Anduin2017 <anduin@aiursoft.com>
2019-11-01 15:00:59 +0800

    Add attribute is guid.

---
+++ Pylon/Attributes/IsGuid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aiursoft.Pylon.Attributes
+{
+    public class IsGuid : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is string val)
+            {
+                return Guid.TryParse(val, out _);
+            }
+            return true;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (IsValid(value))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult($"The {validationContext.DisplayName} is not a valid GUID value!");
+            }
+        }
+    }
+}
