016f82851fe4a5c85b946159e6106267302bd7e7
Anduin2017 <anduin@aiursoft.com>
2019-12-05 16:57:36 +0800

    Fix build issues.

--- Pylon/Services/Authentication/IUserDetail.cs
+++ Pylon/Services/Authentication/IUserDetail.cs
@@ -4,17 +4,17 @@ namespace Aiursoft.Pylon.Services.Authentication
 {
     public interface IUserDetail
     {
-        public string Id { get; set; }
-        public string AvatarUrl { get; set; }
+        string Id { get; set; }
+        string AvatarUrl { get; set; }
 
         [StringLength(maximumLength: 25, MinimumLength = 1)]
         [Required(ErrorMessage = "Nickname is required")]
         [Display(Name = "Nickname")]
-        public string Name { get; set; }
-        public string Bio { get; set; }
+        string Name { get; set; }
+        string Bio { get; set; }
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         [Display(Name = "Your new account's Email")]
-        public string Email { get; set; }
+        string Email { get; set; }
     }
 }
