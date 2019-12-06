9402eb741c53a8ce5db60cdfa080138b6c98ecb7
Anduin2017 <anduin@aiursoft.com>
2019-12-06 12:15:51 +0800

    Service location will try to set CDN.

---
+++ Pylon/Services/AsyncHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Pylon.Services
+{
+    public static class AsyncHelper
+    {
+        private static readonly TaskFactory _taskFactory = new
+            TaskFactory(CancellationToken.None,
+                        TaskCreationOptions.None,
+                        TaskContinuationOptions.None,
+                        TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+            => _taskFactory
+                .StartNew(func)
+                .Unwrap()
+                .GetAwaiter()
+                .GetResult();
+
+        public static void RunSync(Func<Task> func)
+            => _taskFactory
+                .StartNew(func)
+                .Unwrap()
+                .GetAwaiter()
+                .GetResult();
+    }
+}
--- Pylon/Services/ServiceLocation.cs
+++ Pylon/Services/ServiceLocation.cs
@@ -1,27 +1,36 @@
 ﻿using Aiursoft.Pylon.Interfaces;
+using DnsClient;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Aiursoft.Pylon.Services
 {
     public class ServiceLocation : ISingletonDependency
     {
-        public readonly string Account;
-        public readonly string Gateway;
-        public readonly string Archon;
-        public readonly string UI;
-        public readonly string Colossus;
-        public readonly string Developer;
-        public readonly string EE;
-        public readonly string Stargate;
-        public readonly string StargateListenAddress;
-        public readonly string Wiki;
-        public readonly string WWW;
-        public readonly string Probe;
-        public readonly string ProbeIO;
-        public readonly string Status;
+        private readonly HttpClient _client;
+        public string Account { get; private set; }
+        public string Gateway { get; private set; }
+        public string Archon { get; private set; }
+        public string UI { get; private set; }
+        public string Colossus { get; private set; }
+        public string Developer { get; private set; }
+        public string EE { get; private set; }
+        public string Stargate { get; private set; }
+        public string StargateListenAddress { get; private set; }
+        public string Wiki { get; private set; }
+        public string WWW { get; private set; }
+        public string Probe { get; private set; }
+        public string ProbeIO { get; private set; }
+        public string Status { get; private set; }
 
-        public ServiceLocation(IConfiguration configuration)
+        public ServiceLocation(
+            IConfiguration configuration,
+            IHttpClientFactory clientFactory)
         {
+            _client = clientFactory.CreateClient();
             var section = configuration.GetSection("Dependencies");
             Account = TrySet(section["AccountPath"], "https://account.aiursoft.com");
             Gateway = TrySet(section["GatewayPath"], "https://gateway.aiursoft.com");
@@ -40,11 +49,62 @@ namespace Aiursoft.Pylon.Services
             StargateListenAddress = Stargate
                 .Replace("https://", "wss://")
                 .Replace("http://", "ws://");
+
+            AsyncHelper.RunSync(async () =>
+            {
+                UI = await TryGetCDNDomain(UI);
+            });
         }
 
         private string TrySet(string setting, string defaultValue)
         {
             return string.IsNullOrEmpty(setting) ? defaultValue : setting;
+        }
+
+        public async Task<string> TryGetCDNDomain(string input)
+        {
+            var domains = input.Split('.').ToList();
+            domains.Insert(1, "cdn");
+
+            var combined = string.Join(".", domains);
+            try
+            {
+                var result = await QueryDNS(combined);
+                if (!result.HasError && result.Answers.Any() && await TryConnect(combined))
+                {
+                    return combined;
+                }
+                return input;
+            }
+            catch
+            {
+                return input;
+            }
+        }
+
+        public async Task<bool> TryConnect(string path)
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, path);
+                var response = await _client.SendAsync(request);
+                return response.IsSuccessStatusCode;
+            }
+            catch (HttpRequestException)
+            {
+                return false;
+            }
+        }
+
+        public async Task<IDnsQueryResponse> QueryDNS(string host)
+        {
+            var httpsRegex = new Regex("^https://", RegexOptions.Compiled);
+            var httpRegex = new Regex("^http://", RegexOptions.Compiled);
+            host = httpsRegex.Replace(host, string.Empty);
+            host = httpRegex.Replace(host, string.Empty);
+            var lookup = new LookupClient();
+            var result = await lookup.QueryAsync(host, QueryType.ANY);
+            return result;
         }
     }
 }
