69089fb019c638fa6670c24d063dbc21b493bcce
Anduin2017 <anduin@aiursoft.com>
2019-12-16 13:43:49 +0800

    Display file size in colossus and developer.

--- Pylon/Services/StringOperation.cs
+++ Pylon/Services/StringOperation.cs
@@ -225,5 +225,17 @@ namespace Aiursoft.Pylon.Services
                 yield return input.Substring(i, Math.Min(partLength, input.Length - i));
             }
         }
+
+        public static string HumanReadableSize(this long size)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            int order = 0;
+            while (size >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                size = size / 1024;
+            }
+            return string.Format("{0:0.##} {1}", size, sizes[order]);
+        }
     }
 }
