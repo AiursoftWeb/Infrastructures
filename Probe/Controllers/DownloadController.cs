79500599c0858db53d9e7707b82c8c603b51a11f
Anduin2017 <anduin@aiursoft.com>
2020-03-31 15:14:44 +0800

    Use cache to speed up probe.

--- Probe/Controllers/DownloadController.cs
+++ Probe/Controllers/DownloadController.cs
@@ -4,6 +4,7 @@ using Aiursoft.Handler.Models;
 using Aiursoft.Probe.Data;
 using Aiursoft.Probe.Services;
 using Aiursoft.Pylon.Services;
+using Aiursoft.SDK.Models.Probe;
 using Aiursoft.SDK.Models.Probe.DownloadAddressModels;
 using Aiursoft.XelNaga.Tools;
 using Microsoft.AspNetCore.Mvc;
@@ -27,31 +28,29 @@ namespace Aiursoft.Probe.Controllers
         private readonly IConfiguration _configuration;
         private readonly ImageCompressor _imageCompressor;
         private readonly TokenEnsurer _tokenEnsurer;
+        private readonly AiurCache _cache;
 
         public DownloadController(
             FolderLocator folderLocator,
             ProbeDbContext dbContext,
             IConfiguration configuration,
             ImageCompressor imageCompressor,
-            TokenEnsurer tokenEnsurer)
+            TokenEnsurer tokenEnsurer,
+            AiurCache cache)
         {
             _folderLocator = folderLocator;
             _dbContext = dbContext;
             _configuration = configuration;
             _imageCompressor = imageCompressor;
             _tokenEnsurer = tokenEnsurer;
+            _cache = cache;
         }
 
         [Route(template: "File/{SiteName}/{**FolderNames}", Name = "File")]
         [Route(template: "Open/{SiteName}/{**FolderNames}", Name = "Open")]
         public async Task<IActionResult> Open(OpenAddressModel model)
         {
-            var site = await _dbContext
-                .Sites
-                .Include(t => t.Root)
-                .Include(t => t.Root.SubFolders)
-                .Include(t => t.Root.Files)
-                .SingleOrDefaultAsync(t => t.SiteName.ToLower() == model.SiteName);
+            var site = await GetSiteWithCache(model.SiteName);
             if (site == null)
             {
                 return NotFound();
@@ -63,8 +62,7 @@ namespace Aiursoft.Probe.Controllers
             var (folders, fileName) = _folderLocator.SplitToPath(model.FolderNames);
             try
             {
-                var folder = await _folderLocator.LocateAsync(folders, site.Root, false);
-                var file = folder.Files.SingleOrDefault(t => t.FileName == fileName);
+                var file = await GetFileWithCache(folders, fileName, site.Root);
                 if (file == null)
                 {
                     return NotFound();
@@ -88,6 +86,27 @@ namespace Aiursoft.Probe.Controllers
             {
                 return NotFound();
             }
+        }
+
+        private Task<Site> GetSiteWithCache(string siteName)
+        {
+            return _cache.GetAndCache($"site.info.{siteName}", () =>
+               _dbContext
+               .Sites
+               .Include(t => t.Root)
+               .Include(t => t.Root.SubFolders)
+               .Include(t => t.Root.Files)
+               .SingleOrDefaultAsync(t => t.SiteName.ToLower() == siteName));
+        }
+
+        public Task<SDK.Models.Probe.File> GetFileWithCache(string[] folders, string fileName, Folder root)
+        {
+            return _cache.GetAndCache($"file.info.{folders}.{fileName}", async () =>
+            {
+                var folder = await _folderLocator.LocateAsync(folders, root, false);
+                var file = folder.Files.SingleOrDefault(t => t.FileName == fileName);
+                return file;
+            });
         }
 
         private async Task<IActionResult> FileWithImageCompressor(string path, string extension)
