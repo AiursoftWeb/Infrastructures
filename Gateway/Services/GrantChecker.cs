ce3a28b329b5376712150c1617f03ce8ac28a980
Anduin Xue <anduin@aiursoft.com>
2020-04-12 22:02:11 +0800

    Cache app info.

--- Gateway/Services/GrantChecker.cs
+++ Gateway/Services/GrantChecker.cs
@@ -3,6 +3,7 @@ using Aiursoft.Gateway.Data;
 using Aiursoft.Gateway.Models;
 using Aiursoft.Handler.Exceptions;
 using Aiursoft.Handler.Models;
+using Aiursoft.Pylon.Services;
 using Aiursoft.Scanner.Interfaces;
 using Aiursoft.SDK.Models.Developer;
 using Aiursoft.SDK.Services.ToDeveloperServer;
@@ -18,23 +19,25 @@ namespace Aiursoft.Gateway.Services
         private readonly GatewayDbContext _dbContext;
         private readonly DeveloperApiService _developerApiService;
         private readonly ACTokenManager _tokenManager;
+        private readonly AiurCache _aiurCache;
 
         public GrantChecker(
             GatewayDbContext context,
             DeveloperApiService developerApiService,
-            ACTokenManager tokenManager)
+            ACTokenManager tokenManager,
+            AiurCache aiurCache)
         {
             _dbContext = context;
             _developerApiService = developerApiService;
             _tokenManager = tokenManager;
+            _aiurCache = aiurCache;
         }
 
-#warning consider cache here.
         public async Task<GatewayUser> EnsureGranted(string accessToken, string userId, Func<App, bool> prefix)
         {
             var appid = await _tokenManager.ValidateAccessToken(accessToken);
             var targetUser = await _dbContext.Users.Include(t => t.Emails).SingleOrDefaultAsync(t => t.Id == userId);
-            var app = await _developerApiService.AppInfoAsync(appid);
+            var app = await _aiurCache.GetAndCache($"app-info-cache-{appid}", () => _developerApiService.AppInfoAsync(appid));
             if (!_dbContext.LocalAppGrant.Any(t => t.AppID == appid && t.GatewayUserId == targetUser.Id))
             {
                 throw new AiurAPIModelException(ErrorType.Unauthorized, "This user did not grant your app!");
