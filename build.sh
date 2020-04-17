a3911a385a60f76d805c16cf840ed97e070b96f2
Anduin Xue <anduin@aiursoft.com>
2020-04-18 01:58:16 +0800

    Fix build script.

--- build.sh
+++ build.sh
@@ -1,45 +1,4 @@
 echo start building...
-
 dotnet restore
-
-cd SDK
-dotnet build
-cd ..
-
-cd Pylon
-dotnet build
-cd ..
-
-cd Gateway
-dotnet publish --no-restore -c Release
-cd ..
-
-cd Archon
-dotnet publish --no-restore -c Release
-cd ..
-
-cd Account
-dotnet publish --no-restore -c Release
-cd ..
-
-cd Stargate
-dotnet publish --no-restore -c Release
-cd ..
-
-cd Wiki
-dotnet publish --no-restore -c Release
-cd ..
-
-cd EE
-dotnet publish --no-restore -c Release
-cd ..
-
-cd Colossus
-dotnet publish --no-restore -c Release
-cd ..
-
-cd WWW
-dotnet publish --no-restore -c Release
-cd ..
-
+find . -name "*.csproj"  -exec bash -c "dotnet publish '{}' -c Release --no-restore" \;
 exit
