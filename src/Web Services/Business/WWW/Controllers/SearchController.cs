9706992f395fd5d77b30a6f29ab8a546347deee1
Anduin Xue <anduin@aiursoft.com>
2020-04-19 01:36:54 +0800

    Search style home page.

---
+++ src/Web Services/Business/WWW/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using Aiursoft.Handler.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Aiursoft.WWW.Controllers
+{
+    [LimitPerMin]
+    public class SearchController : Controller
+    {
+
+        [Route("search")]
+        public async Task<IActionResult> DoSearch([FromQuery(Name = "q")]string question)
+        {
+            return Json(new { q = question });
+        }
+    }
+}
