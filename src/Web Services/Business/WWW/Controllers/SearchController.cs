32720557c3fc151797902e67830217eb2bfd10e5
Anduin Xue <anduin@aiursoft.com>
2020-04-19 02:15:22 +0800

    Real search service.

--- src/Web Services/Business/WWW/Controllers/SearchController.cs
+++ src/Web Services/Business/WWW/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
 ﻿using Aiursoft.Handler.Attributes;
+using Aiursoft.WWW.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -7,11 +8,33 @@ namespace Aiursoft.WWW.Controllers
     [LimitPerMin]
     public class SearchController : Controller
     {
+        private readonly SearchService _searchService;
+
+        public SearchController(SearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
 
         [Route("search")]
         public async Task<IActionResult> DoSearch([FromQuery(Name = "q")]string question)
         {
-            return Json(new { q = question });
+            var result = await _searchService.DoSearch(question);
+            return View(result);
+        }
+
+        [Route("searchraw")]
+        public async Task<IActionResult> SearchRaw([FromQuery(Name = "q")]string question)
+        {
+            var result = await _searchService.DoSearch(question);
+            return Json(result);
+        }
+
+        [Route("opensearch")]
+        public IActionResult OpenSearch()
+        {
+            Response.ContentType = "text/xml";
+            return View();
         }
     }
 }
---
+++ src/Web Services/Business/WWW/Services/SearchService.cs
@@ -0,0 +1,26 @@
+﻿using Aiursoft.Scanner.Interfaces;
+using Microsoft.Azure.CognitiveServices.Search.WebSearch;
+using Microsoft.Azure.CognitiveServices.Search.WebSearch.Models;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Aiursoft.WWW.Services
+{
+    public class SearchService : ISingletonDependency
+    {
+        private readonly string _searchAPIKey;
+        private readonly WebSearchClient _client;
+
+        public SearchService(IConfiguration configuration)
+        {
+            _searchAPIKey = configuration["BingSearchAPIKey"];
+            _client = new WebSearchClient(new ApiKeyServiceClientCredentials(_searchAPIKey));
+        }
+
+        public async Task<SearchResponse> DoSearch(string question)
+        {
+            var webData = await _client.Web.SearchAsync(query: question);
+            return webData;
+        }
+    }
+}
