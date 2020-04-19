a5d5d4996d5051f1f2dd1b650f47c0f4f136769e
Anduin Xue <anduin@aiursoft.com>
2020-04-19 14:13:06 +0800

    Better componized.

---
+++ src/Web Services/Business/WWW/Views/Shared/Components/Images/Images.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aiursoft.Pylon.Views.Shared.Components.AiurFooter
+{
+    public class Images : ViewComponent
+    {
+        public IViewComponentResult Invoke(Microsoft.Azure.CognitiveServices.Search.WebSearch.Models.Images images)
+        {
+            return View(model: images);
+        }
+    }
+}
---
+++ src/Web Services/Business/WWW/Views/Shared/Components/Videos/Videos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aiursoft.Pylon.Views.Shared.Components.AiurFooter
+{
+    public class Videos : ViewComponent
+    {
+        public IViewComponentResult Invoke(Microsoft.Azure.CognitiveServices.Search.WebSearch.Models.Videos videos)
+        {
+            return View(model: videos);
+        }
+    }
+}
