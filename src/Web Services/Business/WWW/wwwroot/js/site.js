22767e7365431ce7820e002602beab1278cec5ce
Anduin Xue <anduin@aiursoft.com>
2020-04-19 15:23:12 +0800

    Hover video to play

---
+++ src/Web Services/Business/WWW/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    var tempHtmlStorage = "";
+    var hovering = false;
+    $('*[data-hover-video]').each(function () {
+        var placehoder = $(this);
+        var innerPlace = $(this).find("[data-place]");
+        placehoder.hover(function () {
+            if (hovering) return;
+            hovering = true;
+            tempHtmlStorage = innerPlace.html();
+            innerPlace.html('<video id="preview" loop="loop" autoplay="autoplay" muted="muted" style="width:100%" src="' + placehoder.attr('data-hover-video') + '"></video>');
+            setTimeout(function () {
+                document.getElementById('preview').addEventListener('loadedmetadata', function () {
+                    this.currentTime = 0;
+                }, false);
+            }, 1);
+        },
+            function () {
+                hovering = false;
+                innerPlace.html(tempHtmlStorage);
+            });
+    });
+});
