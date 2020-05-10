9d207e5c5677bc7666c946228388b5b72102969d
Anduin Xue <anduin@aiursoft.com>
2020-05-10 15:56:02 +0800

    Move out app repo.

--- src/Web Services/Infrastructure/Probe/Controllers/SitesController.cs
+++ src/Web Services/Infrastructure/Probe/Controllers/SitesController.cs
@@ -1,5 +1,4 @@
-﻿using Aiursoft.Archon.SDK.Services;
-using Aiursoft.DBTools;
+﻿using Aiursoft.DBTools;
 using Aiursoft.DocGenerator.Attributes;
 using Aiursoft.Handler.Attributes;
 using Aiursoft.Handler.Models;
@@ -22,34 +21,23 @@ namespace Aiursoft.Probe.Controllers
     public class SitesController : Controller
     {
         private readonly ProbeDbContext _dbContext;
-        private readonly ACTokenManager _tokenManager;
+        private readonly AppRepo _appRepo;
         private readonly FolderRepo _folderRepo;
 
         public SitesController(
             ProbeDbContext dbContext,
-            ACTokenManager tokenManager,
+            AppRepo appRepo,
             FolderRepo folderRepo)
         {
             _dbContext = dbContext;
-            _tokenManager = tokenManager;
+            _appRepo = appRepo;
             _folderRepo = folderRepo;
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateNewSite(CreateNewSiteAddressModel model)
         {
-            var appid = await _tokenManager.ValidateAccessToken(model.AccessToken);
-            var appLocal = await _dbContext.Apps.SingleOrDefaultAsync(t => t.AppId == appid);
-            if (appLocal == null)
-            {
-                appLocal = new ProbeApp
-                {
-                    AppId = appid
-                };
-                _dbContext.Apps.Add(appLocal);
-                await _dbContext.SaveChangesAsync();
-            }
-
+            var appid = await _appRepo.GetAppId(model.AccessToken);
             var conflict = await _dbContext.Sites
                 .AnyAsync(t => t.SiteName.ToLower().Trim() == model.NewSiteName.ToLower().Trim());
             if (conflict)
@@ -78,18 +66,7 @@ namespace Aiursoft.Probe.Controllers
         [APIProduces(typeof(ViewMySitesViewModel))]
         public async Task<IActionResult> ViewMySites(ViewMySitesAddressModel model)
         {
-            var appid = await _tokenManager.ValidateAccessToken(model.AccessToken);
-            var appLocal = await _dbContext.Apps.SingleOrDefaultAsync(t => t.AppId == appid);
-            if (appLocal == null)
-            {
-                appLocal = new ProbeApp
-                {
-                    AppId = appid
-                };
-                _dbContext.Apps.Add(appLocal);
-                await _dbContext.SaveChangesAsync();
-            }
-
+            var appid = await _appRepo.GetAppId(model.AccessToken);
             var sites = await _dbContext
                 .Sites
                 .Where(t => t.AppId == appid)
@@ -97,7 +74,7 @@ namespace Aiursoft.Probe.Controllers
                 .ToListAsync();
             var viewModel = new ViewMySitesViewModel
             {
-                AppId = appLocal.AppId,
+                AppId = appid,
                 Sites = sites,
                 Code = ErrorType.Success,
                 Message = "Successfully get your buckets!"
@@ -108,17 +85,7 @@ namespace Aiursoft.Probe.Controllers
         [APIProduces(typeof(ViewSiteDetailViewModel))]
         public async Task<IActionResult> ViewSiteDetail(ViewSiteDetailAddressModel model)
         {
-            var appid = await _tokenManager.ValidateAccessToken(model.AccessToken);
-            var appLocal = await _dbContext.Apps.SingleOrDefaultAsync(t => t.AppId == appid);
-            if (appLocal == null)
-            {
-                appLocal = new ProbeApp
-                {
-                    AppId = appid
-                };
-                _dbContext.Apps.Add(appLocal);
-                await _dbContext.SaveChangesAsync();
-            }
+            var appid = await _appRepo.GetAppId(model.AccessToken);
 
             var site = await _dbContext
                 .Sites
@@ -131,7 +98,7 @@ namespace Aiursoft.Probe.Controllers
             }
             var viewModel = new ViewSiteDetailViewModel
             {
-                AppId = appLocal.AppId,
+                AppId = appid,
                 Site = site,
                 Size = await _folderRepo.GetFolderSize(site.RootFolderId),
                 Code = ErrorType.Success,
@@ -143,7 +110,7 @@ namespace Aiursoft.Probe.Controllers
         [HttpPost]
         public async Task<IActionResult> UpdateSiteInfo(UpdateSiteInfoAddressModel model)
         {
-            var appid = await _tokenManager.ValidateAccessToken(model.AccessToken);
+            var appid = await _appRepo.GetAppId(model.AccessToken);
             var site = await _dbContext
                 .Sites
                 .Include(t => t.Root)
@@ -173,7 +140,7 @@ namespace Aiursoft.Probe.Controllers
         [HttpPost]
         public async Task<IActionResult> DeleteSite(DeleteSiteAddressModel model)
         {
-            var appid = await _tokenManager.ValidateAccessToken(model.AccessToken);
+            var appid = await _appRepo.GetAppId(model.AccessToken);
             var site = await _dbContext
                 .Sites
                 .SingleOrDefaultAsync(t => t.SiteName == model.SiteName);
@@ -194,7 +161,7 @@ namespace Aiursoft.Probe.Controllers
         [HttpPost]
         public async Task<IActionResult> DeleteApp(DeleteAppAddressModel model)
         {
-            var appid = await _tokenManager.ValidateAccessToken(model.AccessToken);
+            var appid = await _appRepo.GetAppId(model.AccessToken);
             if (appid != model.AppId)
             {
                 return this.Protocol(ErrorType.Unauthorized, "The app you try to delete is not the access token you granted!");
---
+++ src/Web Services/Infrastructure/Probe/Repositories/AppRepo.cs
@@ -0,0 +1,38 @@
+﻿using Aiursoft.Archon.SDK.Services;
+using Aiursoft.Probe.Data;
+using Aiursoft.Probe.SDK.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Aiursoft.Probe.Repositories
+{
+    public class AppRepo
+    {
+        private readonly ACTokenManager _acTokenManager;
+        private readonly ProbeDbContext _dbContext;
+
+        public AppRepo(
+            ACTokenManager acTokenManager,
+            ProbeDbContext dbContext)
+        {
+            _acTokenManager = acTokenManager;
+            _dbContext = dbContext;
+        }
+
+        public async Task<string> GetAppId(string accessToken)
+        {
+            var appid = await _acTokenManager.ValidateAccessToken(accessToken);
+            var appLocal = await _dbContext.Apps.SingleOrDefaultAsync(t => t.AppId == appid);
+            if (appLocal == null)
+            {
+                appLocal = new ProbeApp
+                {
+                    AppId = appid
+                };
+                _dbContext.Apps.Add(appLocal);
+                await _dbContext.SaveChangesAsync();
+            }
+            return appLocal.AppId;
+        }
+    }
+}
