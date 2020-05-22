5d9fc4cc0ccce3078df61176ddbe9977bb3dbe5b
Anduin Xue <anduin@aiursoft.com>
2020-05-22 23:59:55 +0800

    Delete useless files for observer.

--- src/Web Services/Infrastructure/Observer/Models/HomeViewModels/IndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Aiursoft.Observer.Models.HomeViewModels
-{
-    public class IndexViewModel
-    {
-        public List<MonitorRule> Data { get; internal set; }
-    }
-}
