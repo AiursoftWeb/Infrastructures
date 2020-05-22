88981397c7061e0760cba594ceca4797c7c94183
Anduin Xue <anduin@aiursoft.com>
2020-05-23 00:05:11 +0800

    Migrate project.

--- src/Web Services/Business/Status/Migrations/20200522160413_SplitProjects.Designer.cs
+++ src/Web Services/Business/Status/Migrations/20200522160413_SplitProjects.Designer.cs
@@ -4,14 +4,16 @@ using Aiursoft.Status.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Aiursoft.Status.Migrations
 {
     [DbContext(typeof(StatusDbContext))]
-    partial class StatusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200522160413_SplitProjects")]
+    partial class SplitProjects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Web Services/Business/Status/Migrations/20200522160413_SplitProjects.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aiursoft.Status.Migrations
+{
+    public partial class SplitProjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ErrorLogs");
+
+            migrationBuilder.DropTable(
+                name: "StatusApps");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StatusApps",
+                columns: table => new
+                {
+                    AppId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusApps", x => x.AppId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ErrorLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    EventLevel = table.Column<int>(type: "int", nullable: false),
+                    LogTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ErrorLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ErrorLogs_StatusApps_AppId",
+                        column: x => x.AppId,
+                        principalTable: "StatusApps",
+                        principalColumn: "AppId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ErrorLogs_AppId",
+                table: "ErrorLogs",
+                column: "AppId");
+        }
+    }
+}
