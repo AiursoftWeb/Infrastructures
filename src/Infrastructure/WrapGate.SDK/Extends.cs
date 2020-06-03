44c420a74d98e0adb8751a41f74ba4f8f51c4409
Anduin Xue <anduin@aiursoft.com>
2020-06-03 19:43:49 +0800

    Create wrapgate sdk project.

---
+++ src/Infrastructure/WrapGate.SDK/Extends.cs
@@ -0,0 +1,13 @@
+﻿using Aiursoft.WrapGate.SDK.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aiursoft.WrapGate.SDK
+{
+    public static class Extends
+    {
+        public static IServiceCollection AddWrapGateServer(this IServiceCollection services, string wrapgateEndpoint)
+        {
+            return services.AddSingleton(new WrapGateLocator(wrapgateEndpoint));
+        }
+    }
+}
--- src/Web Services/Infrastructure/WrapGate/Startup.cs
+++ src/Web Services/Infrastructure/WrapGate/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
 using Aiursoft.Archon.SDK;
 using Aiursoft.Observer.SDK;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
+using Aiursoft.SDK;
 using Aiursoft.WrapGate.Data;
+using Aiursoft.WrapGate.SDK.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Aiursoft.SDK;
 
 namespace Aiursoft.WrapGate
 {
@@ -31,8 +26,8 @@ namespace Aiursoft.WrapGate
             services.AddAiurAPIMvc();
             services.AddArchonServer(Configuration.GetConnectionString("ArchonConnection"));
             services.AddObserverServer(Configuration.GetConnectionString("ObserverConnection"));
+            services.AddSingleton(new WrapGateLocator(Configuration["WrapgateEndpoint"]));
             services.AddBasic();
-#warning Add itself endpoint service as singleton!
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
