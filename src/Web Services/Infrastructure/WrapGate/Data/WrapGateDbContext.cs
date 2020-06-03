438c1e17d304163a7696ff4c29a14dc27a97bc8f
Anduin Xue <anduin@aiursoft.com>
2020-06-03 19:31:52 +0800

    Create new project wrap gate.

---
+++ src/Web Services/Infrastructure/WrapGate/Data/WrapGateDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aiursoft.WrapGate.Data
+{
+    public class WrapGateDbContext : DbContext
+    {
+        public WrapGate(DbContextOptions<WrapGate> options): base(options)
+        {
+        }
+    }
+}
---
+++ src/Web Services/Infrastructure/WrapGate/Startup.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Aiursoft.Archon.SDK;
+using Aiursoft.Observer.SDK;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Aiursoft.WrapGate.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Aiursoft.SDK;
+
+namespace Aiursoft.WrapGate
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextWithCache<WrapGateDbContext>(Configuration.GetConnectionString("DatabaseConnection"));
+
+            services.AddAiurAPIMvc();
+            services.AddArchonServer(Configuration.GetConnectionString("ArchonConnection"));
+            services.AddObserverServer(Configuration.GetConnectionString("ObserverConnection"));
+            services.AddBasic();
+#warning Add itself endpoint service as singleton!
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseAiurAPIHandler(env.IsDevelopment());
+            app.UseAiursoftAPIDefault();
+        }
+    }
+}
