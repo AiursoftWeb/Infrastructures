a272d042d260c1269fcad050c8a0880ecb73f3c7
Anduin Xue <anduin@aiursoft.com>
2020-06-06 00:23:40 +0800

    Migrate.

--- src/Web Services/Infrastructure/Wrapgate/Migrations/20200605162334_RichModel.Designer.cs
+++ src/Web Services/Infrastructure/Wrapgate/Migrations/20200605162334_RichModel.Designer.cs
@@ -3,14 +3,16 @@ using Aiursoft.Wrapgate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Aiursoft.Wrapgate.Migrations
 {
     [DbContext(typeof(WrapgateDbContext))]
-    partial class WrapgateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200605162334_RichModel")]
+    partial class RichModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
