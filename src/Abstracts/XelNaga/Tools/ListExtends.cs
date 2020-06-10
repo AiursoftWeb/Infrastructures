65ab6d2047aad15bd6547d276ea2517304de02c9
Anduin Xue <anduin@aiursoft.com>
2020-06-10 10:38:19 +0800

    Add shuf method.

---
+++ src/Abstracts/XelNaga/Tools/ListExtends.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aiursoft.XelNaga.Tools
+{
+    public static class ListExtends
+    {
+        private static Random _rng = new Random();
+        public static IList<T> Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = _rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list;
+        }
+    }
+}
