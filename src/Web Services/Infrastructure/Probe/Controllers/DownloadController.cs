129707c6ccfe9dd59f6c781c048eb46083a804b0
Anduin Xue <anduin@aiursoft.com>
2020-07-05 23:51:51 +0800

    Add a video player for probe.

--- src/Web Services/Infrastructure/Probe/Controllers/DownloadController.cs
+++ src/Web Services/Infrastructure/Probe/Controllers/DownloadController.cs
@@ -3,17 +3,19 @@ using Aiursoft.Handler.Exceptions;
 using Aiursoft.Handler.Models;
 using Aiursoft.Probe.Repositories;
 using Aiursoft.Probe.SDK.Models.DownloadAddressModels;
+using Aiursoft.Probe.SDK.Services;
 using Aiursoft.Probe.Services;
 using Aiursoft.XelNaga.Tools;
 using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp;
+using System;
 using System.Threading.Tasks;
 
 namespace Aiursoft.Probe.Controllers
 {
     [Route("Download")]
     [APIExpHandler]
-    public class DownloadController : ControllerBase
+    public class DownloadController : Controller
     {
         private readonly FolderSplitter _folderSplitter;
         private readonly ImageCompressor _imageCompressor;
@@ -22,6 +24,7 @@ namespace Aiursoft.Probe.Controllers
         private readonly FolderRepo _folderRepo;
         private readonly FileRepo _fileRepo;
         private readonly SiteRepo _siteRepo;
+        private readonly ProbeLocator probeLocator;
 
         public DownloadController(
             FolderSplitter folderLocator,
@@ -30,7 +33,8 @@ namespace Aiursoft.Probe.Controllers
             IStorageProvider storageProvider,
             FolderRepo folderRepo,
             FileRepo fileRepo,
-            SiteRepo siteRepo)
+            SiteRepo siteRepo,
+            ProbeLocator probeLocator)
         {
             _folderSplitter = folderLocator;
             _imageCompressor = imageCompressor;
@@ -39,10 +43,12 @@ namespace Aiursoft.Probe.Controllers
             _folderRepo = folderRepo;
             _fileRepo = fileRepo;
             _siteRepo = siteRepo;
+            this.probeLocator = probeLocator;
         }
 
         [Route(template: "File/{SiteName}/{**FolderNames}", Name = "File")]
         [Route(template: "Open/{SiteName}/{**FolderNames}", Name = "Open")]
+        [Route(template: "Video/{SiteName}/{**FolderNames}", Name = "Video")]
         public async Task<IActionResult> Open(OpenAddressModel model)
         {
             var site = await _siteRepo.GetSiteByName(model.SiteName);
@@ -74,6 +80,10 @@ namespace Aiursoft.Probe.Controllers
                 {
                     return this.WebFile(path, "do-not-open");
                 }
+                else if (ControllerContext.ActionDescriptor.AttributeRouteInfo.Name == "Video")
+                {
+                    return VideoPlayerWithFile(probeLocator.GetProbeOpenAddress(path));
+                }
                 else if (file.FileName.IsStaticImage() && Image.DetectFormat(path) != null)
                 {
                     return await FileWithImageCompressor(path, extension);
@@ -87,6 +97,11 @@ namespace Aiursoft.Probe.Controllers
             {
                 return NotFound();
             }
+        }
+
+        private IActionResult VideoPlayerWithFile(string path)
+        {
+            return View("Player", path);
         }
 
         private async Task<IActionResult> FileWithImageCompressor(string path, string extension)
