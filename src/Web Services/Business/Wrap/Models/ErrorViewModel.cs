2ac3be10450d10a0c6955139c0ae0d0fffffafe0
Anduin Xue <anduin@aiursoft.com>
2020-07-12 22:11:37 +0800

    Add wrap project.

---
+++ src/Web Services/Business/Wrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aiursoft.Wrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
