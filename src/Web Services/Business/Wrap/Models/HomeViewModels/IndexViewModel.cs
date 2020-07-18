3bdfe4f717027b19a3c1bd07464c0e24a10bb9f8
Anduin Xue <anduin@aiursoft.com>
2020-07-18 21:14:26 +0800

    Better wrap home page.

--- src/Web Services/Business/Wrap/Models/HomeViewModels/IndexViewModel.cs
+++ src/Web Services/Business/Wrap/Models/HomeViewModels/IndexViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Aiursoft.SDKTools.Attributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,16 @@ namespace Aiursoft.Wrap.Models.HomeViewModels
 {
     public class IndexViewModel
     {
+        [Required]
+        [ValidDomainName]
+        [MaxLength(50)]
+        [MinLength(5)]
+        public string NewRecordName { get; set; } = Guid.NewGuid().ToString("N").Substring(0, 7);
+
+        [Required]
+        [MaxLength(1000)]
+        [MinLength(5)]
+        [Url]
+        public string Url { get; set; }
     }
 }
