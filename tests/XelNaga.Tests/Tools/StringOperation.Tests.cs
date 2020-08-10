3c7ed9b67fbf38f7bd21a87a71e1185877732e50
Anduin Xue <xuef@microsoft.com>
2020-08-10 19:50:28 +0800

    add test.

---
+++ tests/XelNaga.Tests/Tools/StringOperation.Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Aiursoft.XelNaga.Tools;
+
+namespace Aiursoft.XelNaga.Tests.Tools
+{
+    [TestClass]
+    public class StringOperation
+    {
+        [TestMethod]
+        public void BytesToBase64()
+        {
+            var bytes = Encoding.ASCII.GetBytes("my_test_string");
+
+            var result = bytes.BytesToBase64();
+
+            Assert.AreEqual(result, "bXlfdGVzdF9zdHJpbmc=");
+        }
+    }
+}
