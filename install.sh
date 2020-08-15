c9cf0b4850a4ed1188a233b192f8a31778502601
Anduin Xue <xuef@microsoft.com>
2020-08-15 19:09:15 +0800

    Refactor folders.

--- install.sh
+++ install.sh
@@ -1,9 +1,38 @@
 aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://github.com/AiursoftWeb/AiurScript/raw/master/$1.sh | sudo bash -s $arg; }
 
-kahla_path="/opt/apps/KahlaServer"
+nexus_code="./Nexus"
+nexus_path="/opt/apps/Nexus"
 dbPassword=$(uuidgen)
-port=$(aiur network/get_port)
-connectionString="Server=tcp:127.0.0.1,1433;Database=Kahla;uid=sa;Password=$dbPassword;MultipleActiveResultSets=True;"
+
+archon_path="$nexus_path/Archon"
+gateway_path="$nexus_path/Gateway"
+developer_path="$nexus_path/Developer"
+observer_path="$nexus_path/Observer"
+probe_path="$nexus_path/Probe"
+stargate_path="$nexus_path/Stargate"
+wrapgate_path="$nexus_path/Wrapgate"
+www_path="$nexus_path/WWW"
+wiki_path="$nexus_path/Wiki"
+status_path="$nexus_path/Status"
+account_path="$nexus_path/Account"
+colossus_path="$nexus_path/Colossus"
+wrap_path="$nexus_path/Wrap"
+ee_path="$nexus_path/EE"
+
+archon_code="$nexus_code/src/Web\ Services/Basic/Archon"
+gateway_code="$nexus_code/src/Web\ Services/Basic/Gateway"
+developer_code="$nexus_code/src/Web\ Services/Basic/Developer"
+observer_code="$nexus_code/src/Web\ Services/Infrastructure/Observer"
+probe_code="$nexus_code/src/Web\ Services/Infrastructure/Probe"
+stargate_code="$nexus_code/src/Web\ Services/Infrastructure/Stargate"
+wrapgate_code="$nexus_code/src/Web\ Services/Infrastructure/Wrapgate"
+www_code="$nexus_code/src/Web\ Services/Business/WWW"
+wiki_code="$nexus_code/src/Web\ Services/Business/Wiki"
+status_code="$nexus_code/src/Web\ Services/Business/Status"
+account_code="$nexus_code/src/Web\ Services/Business/Account"
+colossus_code="$nexus_code/src/Web\ Services/Business/Colossus"
+wrap_code="$nexus_code/src/Web\ Services/Business/Wrap"
+ee_code="$nexus_code/src/Web\ Services/Business/EE"
 
 install_nexus()
 {
@@ -15,54 +44,27 @@ install_nexus()
         return 9
     fi
 
-    if [[ $(curl -s https://archon.aiursoft.com/API/AccessToken?appId=$2\&appSecret=$3) == *":0"* ]]; 
-    then
-        echo "App test passed."
-    else
-        echo "AppId and AppSecret for Aiursoft Developer Center is not valid! Please register an valid app at https://developer.aiursoft.com"
-        return 9
-    fi
+    curl -sL https://github.com/AiursoftWeb/AiurUI/raw/master/install.sh | bash -s ui.$1
 
-    aiur network/enable_bbr
     aiur system/set_aspnet_prod
-    aiur install/caddy
     aiur install/dotnet
-    aiur install/node
     aiur install/jq
     aiur install/sql_server
     aiur mssql/config_password $dbPassword
-    aiur git/clone_to AiursoftWeb/Nexus ./Nexus
+    aiur git/clone_to AiursoftWeb/Nexus $nexus_code
     # dotnet publish -c Release -o $kahla_path ./Kahla/Kahla.Server/Kahla.Server.csproj && rm ./Kahla -rf
     # cat $kahla_path/appsettings.json > $kahla_path/appsettings.Production.json
 
-    # npm install web-push -g
-    # web-push generate-vapid-keys > ./temp.txt
-    # publicKey=$(cat ./temp.txt | sed -n 5p)
-    # privateKey=$(cat ./temp.txt | sed -n 8p)
-    # rm ./temp.txt
-
-    # aiur text/edit_json "VapidKeys.PublicKey" "$publicKey" $kahla_path/appsettings.Production.json
-    # aiur text/edit_json "VapidKeys.PrivateKey" "$privateKey" $kahla_path/appsettings.Production.json
     # aiur text/edit_json "ConnectionStrings.DatabaseConnection" "$connectionString" $kahla_path/appsettings.Production.json
-    # aiur text/edit_json "AppDomain[2].Server" "$1" $kahla_path/appsettings.Production.json
     # aiur text/edit_json "KahlaAppId" "$2" $kahla_path/appsettings.Production.json
     # aiur text/edit_json "KahlaAppSecret" "$3" $kahla_path/appsettings.Production.json
-    # aiur text/edit_json "UserIconsSiteName" "$(uuidgen)" $kahla_path/appsettings.Production.json
-    # aiur text/edit_json "UserFilesSiteName" "$(uuidgen)" $kahla_path/appsettings.Production.json
     # aiur services/register_aspnet_service "kahla" $port $kahla_path "Kahla.Server"
     # aiur caddy/add_proxy $1 $port
-    aiur firewall/enable_firewall
-    aiur firewall/open_port 443
-    aiur firewall/open_port 80
 
     # Finish the installation
-    echo "Successfully installed Kahla as a service in your machine! Please open https://$1 to try it now!"
+    echo "Successfully installed Nexus as a service in your machine! Please open https://www.$1 to try it now!"
     echo "The port 1433 is not opened. You can open your database to public via: sudo ufw allow 1433/tcp"
-    echo "You can connect to your server from a Kahla.App. Download the client at: https://www.kahla.app"
     echo "Database identity: $1:1433 with username: sa and password: $dbPassword"
-    echo ""
-    echo "Your database data file is located at: /var/opt/mssql/. Please back up them regularly."
-    echo "Your web data file is located at: $kahla_path"
 }
 
 install_nexus "$@"
