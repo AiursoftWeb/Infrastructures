afd447a588ce8bca76e89586f0bf7ee96681d738
Anduin Xue <xuef@microsoft.com>
2020-08-31 20:38:51 +0800

    Fix terms link.

--- src/SDK/SDK/Values.cs
+++ src/SDK/SDK/Values.cs
@@ -6,5 +6,6 @@ namespace Aiursoft.SDK
     {
         public static string ProjectName = "Aiursoft";
         public static string ProjectValue = "Create a more open world. Aiursoft is focusing on open platform and open communication. Free training, tools, and community to help you grow your skills, career, or business.";
+        public static string TermsAfterWWW = "/docs/terms";
     }
 }
