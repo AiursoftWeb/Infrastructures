278a2c0ceda9693f0a75e7176207e5021315536a
Anduin Xue <xuef@microsoft.com>
2020-09-01 02:00:17 +0800

    Set default mail validated.

--- seed.sql
+++ seed.sql
@@ -9,7 +9,7 @@ insert into AspNetUsers(AccessFailedCount,LockoutEnabled,TwoFactorEnabled,UserNa
 VALUES(0,1,0,'admin@{{domain}}','ADMIN@{{domainUpper}}','AQAAAAEAACcQAAAAEHswFfciW8m5p2De3KzBd7ha0OX04Dav4jRy3CL/M/L54nWtMOGLwDZF1qo211KVRg==','3BO4UKYBY2GJVGNTBM756HO6NQLUAKGQ','89f42d2b-1af9-416a-8953-5abb02b523a1','2020-01-01 12:34:56','{{userId}}','Admin','en-US','admin@{{domain}}','usericon/default.png');
 
 insert into UserEmails(EmailAddress,OwnerId,Priority,Validated,LastSendTime)
-VALUES('admin@{{domain}}','{{userId}}',1,0,'2020-01-01 12:34:56')
+VALUES('admin@{{domain}}','{{userId}}',1,1,'2020-01-01 12:34:56')
 
 use Developer;
 
