0d201a330e6b68e9c0f7f752705056726ea313d0
Anduin Xue <xuef@microsoft.com>
2020-09-29 01:39:38 +0800

    Better qrcode test.

--- tests/WebTools.Tests/Services/QRCodeSerice.Tests.cs
+++ tests/WebTools.Tests/Services/QRCodeSerice.Tests.cs
@@ -18,10 +18,20 @@ namespace Aiursoft.WebTools.Tests.Services
         }
 
         [TestMethod]
-        public void TestQRCode()
+        public void TestChineseQRCode()
         {
             var myCode = _qrCodeService.ToQRCodeBase64("我能吞下玻璃而不伤身体。");
-            Assert.IsTrue(myCode.Length > 10000);
+            Assert.IsTrue(myCode.Length > 20000);
+            Assert.IsTrue(myCode.Length < 50000);
+            Assert.IsTrue(myCode.StartsWith("data:image/png;base64,"));
+        }
+
+        [TestMethod]
+        public void TestURLQRCode()
+        {
+            var myCode = _qrCodeService.ToQRCodeBase64("https://github.com/AiursoftWeb/Infrastructures/blob/dev/src/WebServices/Infrastructure/Wrapgate/Migrations/20200617163030_AddEnabled.Designer.cs");
+            Assert.IsTrue(myCode.Length > 70000);
+            Assert.IsTrue(myCode.Length < 100000);
             Assert.IsTrue(myCode.StartsWith("data:image/png;base64,"));
         }
     }
