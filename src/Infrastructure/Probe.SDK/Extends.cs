4a6618c859a068698f50fe981569c57420b1cb38
Anduin Xue <xuef@microsoft.com>
2020-09-29 02:57:03 +0800

    A better way to fire to start developer center.

--- src/Infrastructure/Probe.SDK/Extends.cs
+++ src/Infrastructure/Probe.SDK/Extends.cs
@@ -53,18 +53,21 @@ namespace Aiursoft.Probe.SDK
             var logger = services.GetRequiredService<ILogger<TProvider>>();
             var siteName = getConfig(configuration);
             var cannon = services.GetService<CannonService>();
-            var tokenProvider = services.GetService(typeof(TProvider)) as TProvider;
-            var token = AsyncHelper.RunSync(() => getToken(tokenProvider));
-            cannon.FireAsync<SitesService>(async (sitesService) =>
+            cannon.Fire<SitesService>((sitesService) =>
             {
-                // Wait 20 seconds. Dependencies might not be started yet.
-                await Task.Delay(20000);
-                logger.LogInformation("Starting create Probe resources...");
-                var sites = await sitesService.ViewMySitesAsync(token);
-                if (!sites.Sites.Any(s => s.SiteName == siteName))
+                cannon.FireAsync<TProvider>(async (tokenProvider) =>
                 {
-                    await sitesService.CreateNewSiteAsync(token, siteName, openToUpload, openToDownload);
-                }
+                    // Wait 20 seconds. Dependencies might not be started yet.
+                    await Task.Delay(20 * 1000);
+                    var token = await getToken(tokenProvider);
+                    logger.LogInformation("Starting create Probe resources...");
+
+                    var sites = await sitesService.ViewMySitesAsync(token);
+                    if (!sites.Sites.Any(s => s.SiteName == siteName))
+                    {
+                        await sitesService.CreateNewSiteAsync(token, siteName, openToUpload, openToDownload);
+                    }
+                });
             });
 
             return host;
