4e4362b9533eb58cf428cd1f69c6207bb45dedd1
Anduin Xue <xuef@microsoft.com>
2020-09-30 00:50:10 +0800

    Really creates tag in wrap.

--- src/WebServices/Basic/Developer/Models/AppsViewModels/ViewAppViewModel.cs
+++ src/WebServices/Basic/Developer/Models/AppsViewModels/ViewAppViewModel.cs
@@ -63,7 +63,7 @@ namespace Aiursoft.Developer.Models.AppsViewModels
             var channels = await channelService.ViewMyChannelsAsync(token);
             Channels = channels.Channels;
 
-            var records = await recordsService.ViewMyRecordsAsync(token);
+            var records = await recordsService.ViewMyRecordsAsync(token, Array.Empty<string>());
             Records = records.Records;
 
             Trusted = appInDb.TrustedApp;
