a90d20489e587920af6454afc3af8bcf2c0ed8cf
Anduin Xue <anduin.xue@microsoft.com>
2020-10-12 19:31:16 +0800

    Allow editting navbar on WWW.

---
+++ src/WebServices/Business/WWW/Models/Navbar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aiursoft.WWW.Models
+{
+    public class Navbar
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public List<Navbar> Dropdowns { get; set; } = new List<Navbar>();
+    }
+}
