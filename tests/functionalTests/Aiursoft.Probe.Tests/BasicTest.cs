a9630e0d0f9bff359b2dc9301f8a63d4303fa22e
Anduin Xue <anduin.xue@microsoft.com>
2021-01-12 15:49:36 +0800

    Create Probe basic functional test.

---
+++ tests/functionalTests/Aiursoft.Probe.Tests/BasicTest.cs
@@ -0,0 +1,65 @@
+﻿using Aiursoft.Handler.Models;
+using Aiursoft.Probe.Data;
+using Aiursoft.Probe.SDK;
+using Aiursoft.Scanner;
+using Aiursoft.SDK;
+using Aiursoft.XelNaga.Tools;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static Aiursoft.WebTools.Extends;
+
+namespace Aiursoft.Stargate.Tests
+{
+    [TestClass]
+    public class BasicTests
+    {
+        private readonly int _port;
+        private readonly string _endpointUrl;
+        private IHost _server;
+        private HttpClient _http;
+        private ServiceProvider _serviceProvider;
+
+        public BasicTests()
+        {
+            _port = Network.GetAvailablePort();
+            _endpointUrl = $"http://localhost:{_port}";
+        }
+
+        [TestInitialize]
+        public async Task CreateServer()
+        {
+            _server = App<TestStartup>(port: _port).Update<ProbeDbContext>(); ;
+            _http = new HttpClient();
+            await _server.StartAsync();
+
+            var services = new ServiceCollection();
+            services.AddHttpClient();
+            services.AddLibraryDependencies();
+            services.AddProbeServer(_endpointUrl);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [TestCleanup]
+        public async Task CleanServer()
+        {
+            await _server.StopAsync();
+            _server.Dispose();
+        }
+
+        [TestMethod]
+        public async Task GetHome()
+        {
+            var response = await _http.GetAsync(_endpointUrl);
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+            var content = await response.Content.ReadAsStringAsync();
+            var contentObject = JsonConvert.DeserializeObject<AiurProtocol>(content);
+            Assert.AreEqual(contentObject.Code, ErrorType.Success);
+        }
+    }
+}
---
+++ tests/functionalTests/Aiursoft.Probe.Tests/TestStartup.cs
@@ -0,0 +1,21 @@
+﻿using Aiursoft.Archon.SDK.Services;
+using Aiursoft.Probe;
+using Aiursoft.SDK.Tests.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Aiursoft.Stargate.Tests
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration) { }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+            services.RemoveAll<ACTokenValidator>();
+            services.AddTransient<ACTokenValidator, MockAcTokenValidator>();
+        }
+    }
+}
