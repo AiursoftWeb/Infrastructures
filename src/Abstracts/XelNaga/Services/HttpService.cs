429f1335e151cf808863f0816722c14d7c958850
Anduin Xue <anduin.xue@microsoft.com>
2021-02-13 18:05:15 +0800

    Upgrade version number.

--- src/Abstracts/XelNaga/Services/HttpService.cs
+++ src/Abstracts/XelNaga/Services/HttpService.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Aiursoft.XelNaga.Services
