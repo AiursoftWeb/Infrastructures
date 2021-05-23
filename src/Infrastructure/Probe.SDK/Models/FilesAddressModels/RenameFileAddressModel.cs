91677964ca08a3a38439364edc78adee097eae5a
Anduin Xue <Anduin@aiursoft.com>
2021-05-23 21:57:56 +0800

    Fix lint issue.

--- src/Infrastructure/Probe.SDK/Models/FilesAddressModels/RenameFileAddressModel.cs
+++ src/Infrastructure/Probe.SDK/Models/FilesAddressModels/RenameFileAddressModel.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aiursoft.Probe.SDK.Models.FilesAddressModels
 {
