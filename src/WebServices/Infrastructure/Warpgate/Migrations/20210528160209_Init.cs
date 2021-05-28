06195b26e5246df67ba5e6fa59f3833faac29eb1
Anduin Xue <Anduin@aiursoft.com>
2021-05-29 00:02:38 +0800

    New database migration scripts.

---
+++ src/WebServices/Infrastructure/Warpgate/Migrations/20210528160209_Init.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aiursoft.Warpgate.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WarpApps",
+                columns: table => new
+                {
+                    AppId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WarpApps", x => x.AppId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    RecordUniqueName = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    TargetUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false),
+                    Tags = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Records_WarpApps_AppId",
+                        column: x => x.AppId,
+                        principalTable: "WarpApps",
+                        principalColumn: "AppId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_AppId",
+                table: "Records",
+                column: "AppId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_RecordUniqueName",
+                table: "Records",
+                column: "RecordUniqueName",
+                unique: true,
+                filter: "[RecordUniqueName] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Records");
+
+            migrationBuilder.DropTable(
+                name: "WarpApps");
+        }
+    }
+}
