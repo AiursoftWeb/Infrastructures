d0dd0c504383fb534366e61a329c46b7dc3f8eb8
Anduin Xue <anduin@aiursoft.com>
2023-05-27 10:00:09 +0000

    Trim usings.

--- src/WebServices/Infrastructure/Warpgate/Controllers/HomeController.cs
+++ src/WebServices/Infrastructure/Warpgate/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using Aiursoft.Handler.Models;
 using Aiursoft.Warpgate.Models.Configuration;
 using Aiursoft.Warpgate.SDK.Models.ViewModels;
-using Aiursoft.Warpgate.SDK.Services;
 using Aiursoft.WebTools;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
