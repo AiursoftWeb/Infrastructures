23ec5719e2240d616fbba3feac9dbeb1dacbca51
Anduin Xue <anduin@aiursoft.com>
2023-07-09 07:56:29 +0000

    Back to migrate.

--- src/WebServices/Basic/Directory/Program.cs
+++ src/WebServices/Basic/Directory/Program.cs
@@ -11,7 +11,7 @@ public class Program
     public static async Task Main(string[] args)
     {
         var app = App<Startup>(args);
-        await app.UpdateDbAsync<DirectoryDbContext>(UpdateMode.RecreateThenUse);
+        await app.UpdateDbAsync<DirectoryDbContext>(UpdateMode.MigrateThenUse);
         // TODO: There should create new Probe Site. But that logic should be in a new project called Gateway.
         await app.RunAsync();
     }
